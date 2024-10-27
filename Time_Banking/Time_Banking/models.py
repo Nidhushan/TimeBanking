f0a192f7489a31f46056362a2e9debbfc2dc7938
frank921 <frankwang200209@gmail.com>
2024-10-27 21:23:06 +0000

    implement create listings

--- Time_Banking/Time_Banking/models.py
+++ Time_Banking/Time_Banking/models.py
@@ -51,27 +51,28 @@ class User(AbstractUser):
         return self.username
 
 # parent class of BOTH offers AND requests
-# class Listing(models.Model):
-#     creator = models.ForeignKey(User, on_delete=models.CASCADE)
-#     category = models.ForeignKey(Category)
-#     tags = models.ManyToManyField(Tag) # backend needs to check that tags belong to category
-#     title = models.CharField() # use CharField
-#     description = models.TextField()
-#     listing_type = models.BooleanField(choices=LISTING_TYPES)
-#     duration = models.DurationField() # how long the work is expected/preferred to take
-#     posted_at = models.DateTimeField(auto_now_add=True)
-#     edited_at = models.DateTimeField(auto_now=True)
-#     def __str__(self):
-#         return self.title
-
-# simple model for Week 5 demo
 class Listing(models.Model):
-    title = models.CharField(max_length=255) # use CharField
-    category = models.CharField(max_length=20, choices=Category.choices) # enum for Category
-    image = models.ImageField(upload_to='static/images/listing')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
+    tags = models.ManyToManyField(Tag) # backend needs to check that tags belong to category
+    title = models.CharField() # use CharField
     description = models.TextField()
+    image = models.ImageField(upload_to='static/images/listing')
+    listing_type = models.BooleanField(choices=LISTING_TYPES)
+    duration = models.DurationField() # how long the work is expected/preferred to take
+    posted_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
+
+# simple model for Week 5 demo
+# class Listing(models.Model):
+#     title = models.CharField(max_length=255) # use CharField
+#     category = models.CharField(max_length=20, choices=Category.choices) # enum for Category
+#     image = models.ImageField(upload_to='static/images/listing')
+#     description = models.TextField()
+#     def __str__(self):
+#         return self.title
 
 # someone responds to an offer or a request
 class ListingResponse(models.Model):
